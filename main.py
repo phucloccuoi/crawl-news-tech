bae09ac0e7f1d69fd36b78b9814b669733e91eaa
Nguyen Phuoc Loc <49280276+phucloccuoi@users.noreply.github.com>
2022-07-06 22:49:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,31 +3,51 @@ from requests_html import HTMLSession, HTML
 from pyppeteer import errors
 from time import sleep
 
-new_session = HTMLSession()
-time_out = 3
-
-try:
-    my_response = new_session.get('https://quantrimang.com/windows-10-os')
+# Hàm tạo một phiên kết nối vào một website với liên kêt cho trước
+def get_session(url_full, time_out):
+    '''
+    - Chức năng: tạo một phiên kết nối vào một website với liên kêt cho trước
+    - url_full: địa chỉ website muốn truy cập
+    - time_out: Thời gian phản hồi của website, sau đó sẽ đóng
+    - result_page: hàm trả về biến loại HTML lưu toàn bộ thông tin html của website
+    '''
+    # Khởi tạo một phiên kết nối
+    my_session = HTMLSession()
 
     try:
+        # Tạo một phiên kết nối tới trang đích
+        my_response = my_session.get(url_full)
+
+        # Tổng thời gian Loading bằng thời gian tối đa là 6s + 2s và tối thiểu là 2s + 2s
+        try:
+            my_response.html.render(scrolldown=1, sleep=time_out, keep_page=True)
+        except ConnectionRefusedError:
+            my_response.html.render(scrolldown=1, sleep=time_out + 1, keep_page=True)
+        except RuntimeError:
+            my_response.html.render(scrolldown=1, sleep=time_out + 2, keep_page=True)
+        except errors.NetworkError:
+            my_response.html.render(scrolldown=1, sleep=time_out - 1, keep_page=True)
+        finally:
+            my_session.close()
+    except errors.TimeoutError:
+        sleep(time_out/2)
         my_response.html.render(scrolldown=1, sleep=time_out, keep_page=True)
-    except ConnectionRefusedError:
-        my_response.html.render(scrolldown=1, sleep=time_out + 1, keep_page=True)
-    except RuntimeError:
-        my_response.html.render(scrolldown=1, sleep=time_out + 2, keep_page=True)
-    except errors.NetworkError:
-        my_response.html.render(scrolldown=1, sleep=time_out - 1, keep_page=True)
     finally:
-            new_session.close()
-except errors.TimeoutError:
-    sleep(time_out/2)
-    my_response.html.render(scrolldown=1, sleep=time_out, keep_page=True)
-finally:
-    new_session.close()
+        my_session.close()
 
-result_page = HTML(html = my_response.html.html)
+    # Lưu kết quả trả về với định dạng theo kiểu HTML
+    result_page = HTML(html = my_response.html.html)
 
-find_link = result_page.find("a.thumb")
+    return result_page
+# Kết thúc hàm tạo một phiên kết nối vào một website với liên kêt cho trước
 
-for index in range(len(find_link)):
-    print(index, find_link[index])
+def main():
+    result_page = get_session(f"https://quantrimang.com/windows-10-os", 4)
+
+    find_link = result_page.find("a.thumb")
+
+    for index in range(len(find_link)):
+        print(index, find_link[index])
+
+if __name__ == "__main__":
+    main()
